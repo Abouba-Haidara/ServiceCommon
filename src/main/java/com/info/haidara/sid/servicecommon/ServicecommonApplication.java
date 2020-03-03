328878b074d2569188a913ca15518c07ea5a5607
Yemini Coder <31940004+Abouba-Haidara@users.noreply.github.com>
2020-03-03 14:44:31 +0000

    Initial commit

---
+++ src/main/java/com/info/haidara/sid/servicecommon/ServicecommonApplication.java
@@ -0,0 +1,13 @@
+package com.info.haidara.sid.servicecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicecommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicecommonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/info/haidara/sid/servicecommon/ServicecommonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.info.haidara.sid.servicecommon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServicecommonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
